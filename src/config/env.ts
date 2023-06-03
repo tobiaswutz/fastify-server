5865331adce44bf218901b30ae6dec71d6224504
Tobias <tobiaswutz22@gmail.com>
2023-06-04 00:37:56 +0200

    port

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,7 +5,7 @@ export const env = zennv({
   dotenv: true,
   schema: z.object({
     PORT: z.number().default(3000),
-    HOST: z.string().default("localhost"),
+    HOST: z.string().default("0.0.0.0"),
     DATABASE_CONNECTION: z.string(),
     JWT_SECRET: z.string(),
   }),
